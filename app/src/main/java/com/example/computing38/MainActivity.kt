0e7483c5ae0f1ff1544be0e64746c4fc8106de0f
Krischal-001 <your_email@example.comkrischallamichhane001@gmail.com>
2026-04-09 11:53:36 +0545

    Feat:Text is added and design is added to it

--- app/src/main/java/com/example/computing38/MainActivity.kt
+++ app/src/main/java/com/example/computing38/MainActivity.kt
@@ -1,16 +1,29 @@
 package com.example.computing38
 
+import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.computing38.ui.theme.Computing38Theme
 
 class MainActivity : ComponentActivity() {
@@ -27,7 +40,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainBody(){
-    Text(text = "Krish_001")
+    Column(modifier = Modifier.fillMaxSize().background(Color.White)){
+        Text(
+            text = "Krish",
+            modifier = Modifier.fillMaxWidth(),
+            style = TextStyle(
+                fontSize = 24.sp,
+                color = Color.Red,
+                fontWeight = FontWeight.W900,
+                fontStyle = FontStyle.Italic,
+                textDecoration = TextDecoration.Underline,
+                textAlign = TextAlign.Center
+            )
+        )
+
+
+
+
+    }
 
 
 }
