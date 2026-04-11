7ce0c7d05fb50efaab37bcee034fd6a5ec4138d6
Krischal-001 <your_email@example.comkrischallamichhane001@gmail.com>
2026-04-12 04:54:59 +0545

    Feat:Top-Section with icons and name is complete

--- app/src/main/java/com/example/computing38/MainActivity.kt
+++ app/src/main/java/com/example/computing38/MainActivity.kt
@@ -1,26 +1,21 @@
 package com.example.computing38
 
-import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -39,30 +34,45 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MainBody(){
-    Column(modifier = Modifier.fillMaxSize().background(Color.White)){
-        Text(
-            text = "Krish",
+fun MainBody() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(15.dp)
+    ) {
+        Row(
             modifier = Modifier.fillMaxWidth(),
-            style = TextStyle(
-                fontSize = 24.sp,
-                color = Color.Red,
-                fontWeight = FontWeight.W900,
-                fontStyle = FontStyle.Italic,
-                textDecoration = TextDecoration.Underline,
-                textAlign = TextAlign.Center
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.baseline_arrow_back_ios_24),
+                contentDescription = "Back Arrow"
             )
-        )
 
+            Text(
+                text = "Krish",
+                style = TextStyle(
+                    fontSize = 24.sp,
+                    color = Color.Black,
+                    fontWeight = FontWeight.W900,
+                    fontStyle = FontStyle.Italic,
+                    textAlign = TextAlign.Center
+                )
+            )
 
-
-
+            Icon(
+                painter = painterResource(id = R.drawable.baseline_more_horiz_24),
+                contentDescription = "Menu"
+            )
+        }
     }
-
-
 }
-@Preview
+
+@Preview(showBackground = true)
 @Composable
-fun MainPreview(){
-    MainBody()
+fun MainPreview() {
+    Computing38Theme {
+        MainBody()
+    }
 }
